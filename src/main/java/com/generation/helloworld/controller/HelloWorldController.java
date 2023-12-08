0cf8b65aa1628463a7b5e6567a9351b6bd44bda8
Pedro-Vhiana <pedrohb.0222@gmail.com>
2023-12-07 21:26:03 -0300

    Tarefa completa

--- src/main/java/com/generation/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -5,27 +5,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/view")
+@RequestMapping("/") // Criando uma porta para o endpoint...
 public class HelloWorldController {
 	
 	
 	
-	@GetMapping("helloworld")
+	@GetMapping("") // Rota raíz == não recebe nada na rota
 	public String helloWorld() {
 		return "Hello World";
 	}
 	
 	@GetMapping("bsm")
 	public String bsm() {
-		return "Bsm da Generation";
+		return "Atenção ao detalhe, Orientação ao futuro, "
+				+ "Persisteência, Trabalho em equipe, Proatividade,"
+				+ "Comunicação, mentalidade de crescimento,"
+				+ "Responsabilidade Pessoal";
 	}
 	
 	@GetMapping("aprendizado")
 	public String aprendizado() {
-		return "Aprendizado";
+		return "Aprimorar os BSMS";
 	}
 	
 
+	@GetMapping("teste")
+	public String teste() {
+		return "testando";
+		
+	}
+	
+	
+	// Localhost:8080/view/"bsm" == end point
 	
 	
 }
